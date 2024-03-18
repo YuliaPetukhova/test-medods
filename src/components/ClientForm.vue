51f3e1429fdac817e38ff64ec042c36007d65438
YuliaPetukhova <petukhova.yulia99@gmail.com>
2024-03-18 17:35:50 +0500

    sass стили

--- src/components/ClientForm.vue
+++ src/components/ClientForm.vue
@@ -327,128 +327,110 @@ export default {
 }
 </script>
 
-<style scoped>
-.first-block, .second-block, .third-block {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 1rem;
-}
+<style scoped lang="sass">
 
-.first-block > div {
-  margin: 5px 5px;
-  flex: 27%;
-}
+.first-block, .second-block, .third-block
+  display: flex
+  flex-wrap: wrap
+  padding: 0 1rem
 
-.second-block > div {
-  margin: 5px 5px;
-  flex: 30%;
-}
+.first-block > div
+  margin: 5px 5px
+  flex: 27%
 
-.third-block > div {
-  margin: 0 5px;
-  flex: 15%;
-}
+.second-block > div
+  margin: 5px 5px
+  flex: 27%
 
-input:not([type='checkbox'], [type='radio']), select {
-  width: 100%;
-  height: 50px;
-  border-radius: 30px;
-  padding: 0 1rem;
-  box-shadow: gray;
-  border: solid 1px gray;
-  text-decoration: none;
-  outline: none;
-}
+.third-block > div
+  margin: 0 5px
+  flex: 27%
 
-label {
-  padding: 0 1rem;
-  font-size: 17px;
-}
+input:not([type='checkbox'], [type='radio']), select
+  width: 100%
+  height: 50px
+  border-radius: 30px
+  padding: 0 1rem
+  box-shadow: gray
+  border: solid 1px gray
+  text-decoration: none
+  outline: none
 
-.submit-btn {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  margin: 5px 0;
-}
+label
+  padding: 0 1rem
+  font-size: 17px
 
+.submit-btn
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  margin: 5px 0
 
-.submit-btn button {
-  font-size: 20px;
-  align-items: center;
-  appearance: none;
-  background-color: #3EB2FD;
-  background-image: linear-gradient(1deg, #4F58FD, #149BF3 99%);
-  background-size: calc(100% + 20px) calc(100% + 20px);
-  border-radius: 100px;
-  border-width: 0;
-  box-shadow: none;
-  box-sizing: border-box;
-  color: #FFFFFF;
-  cursor: pointer;
-  display: inline-flex;
-  height: auto;
-  justify-content: center;
-  line-height: 1.5;
-  padding: 6px 20px;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  transition: background-color .2s,background-position .2s;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  vertical-align: top;
-  white-space: nowrap;
-}
+.submit-btn button
+  font-size: 20px
+  align-items: center
+  appearance: none
+  background-color: #3EB2FD
+  background-image: linear-gradient(1deg, #4F58FD, #149BF3 99%)
+  background-size: calc(100% + 20px) calc(100% + 20px)
+  border-radius: 100px
+  border-width: 0
+  box-shadow: none
+  box-sizing: border-box
+  color: #FFFFFF
+  cursor: pointer
+  display: inline-flex
+  height: auto
+  justify-content: center
+  line-height: 1.5
+  padding: 6px 20px
+  position: relative
+  text-align: center
+  text-decoration: none
+  transition: background-color .2s, background-position .2s
+  user-select: none
+  -webkit-user-select: none
+  touch-action: manipulation
+  vertical-align: top
+  white-space: nowrap
 
 .submit-btn button:active,
-.submit-btn button:focus {
-  outline: none;
-}
+.submit-btn button:focus
+  outline: none
 
-.submit-btn button:hover {
-  background-position: -20px -20px;
-}
+.submit-btn button:hover
+  background-position: -20px -10px
 
-.submit-btn button:focus:not(:active) {
-  box-shadow: rgba(40, 170, 255, 0.25) 0 0 0 .125em;
-}
+.submit-btn button:focus:not(:active)
+  box-shadow: rgba(40, 170, 255, 0.25) 0 0 0 .125em
 
-.sex {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 300px;
-  height: 50px;
-  margin: 5px 0;
-}
+.sex
+  display: flex
+  flex-direction: row
+  align-items: center
+  width: 300px
+  height: 50px
+  margin: 5px 0
 
-.check {
-  height: 20px;
-}
+.check
+  height: 20px
 
-.select {
+.select
   margin-bottom: 10px
-}
 
-.error-message {
-  font-weight: bold;
-  color: #fa3939;
-}
+.error-message
+  font-weight: bold
+  color: #fa3939
 
-@media (max-width: 425px) {
-  .first-block > div {
-    flex: 100%;
-  }
+@media (max-width: 425px)
+  .first-block > div
+    flex: 100%
 
-  .second-block > div {
-    flex: 100%;
-  }
+  .second-block > div
+    flex: 100%
 
-  .third-block > div {
-    flex: 100%;
-  }
-}
+  .third-block > div
+    flex: 100%
+
 </style>
